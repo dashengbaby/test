bb1ee98453a9e1c85b70dbebc2ec19250fb5a786
dashengbaby <3084362216@qq,com>
2024-10-23 13:24:11 +0800

    test

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 30843 on 2024/10/23.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+// Created by 30843 on 2024/10/23.
+//
+
+#ifndef PRJ1_HELLO_H
+#define PRJ1_HELLO_H
+
+
+class hello {
+
+};
+
+
+#endif //PRJ1_HELLO_H
